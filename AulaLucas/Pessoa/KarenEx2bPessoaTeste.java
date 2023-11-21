42d05e1daf108a2c4e46961248fe8e56863533ae
karenvieira12 <115561311+karenvieira12@users.noreply.github.com>
2023-11-21 01:12:23 -0300

    Update KarenEx2bPessoaTeste.java

--- AulaLucas/Pessoa/KarenEx2bPessoaTeste.java
+++ AulaLucas/Pessoa/KarenEx2bPessoaTeste.java
@@ -1,5 +1,15 @@
-package Pessoa;
+public class HerancaExemplo {
+    public static void main(String[] args) {
+        Pobre pessoaPobre = new Pobre("João", 30);
+        pessoaPobre.apresentar();
+        pessoaPobre.trabalhar();
 
-public class KarenEx2bPessoaTeste {
+        Miseravel pessoaMiseravel = new Miseravel("Maria", 25);
+        pessoaMiseravel.apresentar();
+        pessoaMiseravel.pedirEsmola();
 
+        Rica pessoaRica = new Rica("Carlos", 40);
+        pessoaRica.apresentar();
+        pessoaRica.aproveitarVidaLuxuosa();
+    }
 }
