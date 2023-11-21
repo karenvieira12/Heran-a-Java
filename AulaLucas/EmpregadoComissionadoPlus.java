387c3165792e25594b32b18becd5d30dcc3325fe
karenvieira12 <115561311+karenvieira12@users.noreply.github.com>
2023-11-21 01:34:41 -0300

    Update EmpregadoComissionadoPlus.java

--- AulaLucas/EmpregadoComissionadoPlus.java
+++ AulaLucas/EmpregadoComissionadoPlus.java
@@ -1,5 +1,22 @@
-package heranca;
+public class EmpregadoComissionado Plus extends Empregado Comissionado { 
+  private double salarioBase;
 
-public class EmpregadoComissionadoPlus {
+  public EmpregadoComissionado Plus (double salario, String primeiroNome, 
+          String ultimo Nome, String CPF, 
+          double vendabruto, double taxa) {
+          super (primeiro Nome, ultimoNome, CPF, vendabruto, taxa);
+        set SalarioBase (salario);
+   }
+public void setSalarioBase (double salario) {
+  salarioBase (salario < 0.0)70.0: salario;
+}
+public double getSalarioBase () {
+  return salarioBase;
+}
+Override
+
+public double Salario () { //sobrescrevendo o metodo da superclasse
+  return salarioBase + (super.getTaxaComissao() * super.getVendaBruta()); 
+}
 
 }
